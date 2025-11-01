ca107429697b7d80248be0405943a9213fcf4617
BhaumikKhandela <152811705+BhaumikKhandela@users.noreply.github.com>
2025-11-01 13:31:27 +0530

    Update: add Recording complete flag route

---
+++ src/app/api/recording/[id]/complete/route.ts
@@ -0,0 +1,34 @@
+import { client } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest, context: { params: Promise<{id: string}>}){
+
+    try{
+
+    const body = await req.json();
+
+    const { id } = await context.params;
+
+    const completeProcessing = await client.video.update({
+        where:{
+            userId: id,
+            source: body.filename,
+        },
+        data:{
+            processing: false
+        }
+    });
+
+    if(completeProcessing){
+        console.log('🟢 recording processing completed');
+        return NextResponse.json({ status: 200, message: 'Recording processing completed' });
+    }
+
+    return NextResponse.json({ status: 400, message: 'Oops! something went wrong.' });
+
+    } catch(error){
+        console.error('🔴 Error completing recording processing:', error);
+
+        return NextResponse.json({ status: 500, message: 'Internal Server Error' });
+    }
+}
