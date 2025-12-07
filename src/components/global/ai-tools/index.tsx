b1cb14e01c0fc1ea866fa61ba0b655691fc8a0df
BhaumikKhandela <152811705+BhaumikKhandela@users.noreply.github.com>
2025-12-08 02:50:46 +0530

    Moved client side AI Chat Bot component to other file.

--- src/components/global/ai-tools/index.tsx
+++ src/components/global/ai-tools/index.tsx
@@ -2,19 +2,10 @@ import { Button } from "@/components/ui/button";
 import { TabsContent } from "@/components/ui/tabs";
 import React, { useState } from "react";
 import { Loader } from "../loader";
-import { FaVideo } from "react-icons/fa6";
-import {
-  Bot,
-  DownloadIcon,
-  FileTextIcon,
-  Pencil,
-  StarsIcon,
-} from "lucide-react";
+import { Bot, FileTextIcon, Pencil, StarsIcon } from "lucide-react";
 import PaymentButton from "../payment-button";
 import { useAuth } from "@clerk/nextjs";
-import axios from "axios";
-import { toast } from "sonner";
-import { useAIChat } from "@/hooks/useAIChat";
+import { AiChatbot } from "./aichatbot";
 
 type Props = {
   plan: "PRO" | "FREE";
@@ -27,7 +18,6 @@ const AiTools = ({ plan, trial, videoId, clerkId }: Props) => {
   //WIP: setup the ai hook
 
   const { userId } = useAuth();
-
 
   return (
     <TabsContent value="Ai tools">
@@ -74,107 +64,58 @@ const AiTools = ({ plan, trial, videoId, clerkId }: Props) => {
             Download Video <br /> File
           </div>
         </div> */}
-        
-          {
-            userId === clerkId && plan === "FREE" ? (
-              <div className="border-[1px] rounded-xl  p-4 gap-4 flex flex-col bg-[#1b0f1b7f]">
-            <div className="flex items-center gap-2">
-              <h2 className="text-2xl font-bold text-[#a22fe0]"> Opal Ai</h2>
-              <StarsIcon color="#a22fe0" fill="#a22fe0" />
-            </div>
-            <div className="flex gap-2 items-start">
-              <div className="p-2 rounded-full border-[#2d2d2d] border-[2px] bg-[#2b2b2b]">
-                <Pencil color="#a22fe0" />
-              </div>
 
-              <div className="flex flex-col">
-                <h3 className="font-semibold">Summary</h3>
-                <p className="text-muted-foreground text-sm">
-                  Generate a description for your video using AI.
-                </p>
+          {userId === clerkId && plan === "FREE" ? (
+            <div className="border-[1px] rounded-xl  p-4 gap-4 flex flex-col bg-[#1b0f1b7f]">
+              <div className="flex items-center gap-2">
+                <h2 className="text-2xl font-bold text-[#a22fe0]"> Opal Ai</h2>
+                <StarsIcon color="#a22fe0" fill="#a22fe0" />
+              </div>
+              <div className="flex gap-2 items-start">
+                <div className="p-2 rounded-full border-[#2d2d2d] border-[2px] bg-[#2b2b2b]">
+                  <Pencil color="#a22fe0" />
+                </div>
+
+                <div className="flex flex-col">
+                  <h3 className="font-semibold">Summary</h3>
+                  <p className="text-muted-foreground text-sm">
+                    Generate a description for your video using AI.
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-2 items-start">
+                <div className="p-2 rounded-full border-[#2d2d2d] border-[2px] bg-[#2b2b2b]">
+                  <FileTextIcon color="#a22fe0" />
+                </div>
+                <div className="flex flex-col">
+                  <h3 className="font-semibold">Summary</h3>
+                  <p className="text-muted-foreground text-sm">
+                    Generate a description for your video using AI.
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-2 items-start">
+                <div className="p-2 rounded-full border-[#2d2d2d] border-[2px] bg-[#2b2b2b]">
+                  <Bot color="#a22fe0" />
+                </div>
+                <div className="flex flex-col">
+                  <h3 className="font-semibold">AI Agent</h3>
+                  <p className="text-muted-foreground text-sm">
+                    Viewers can ask questions on your video and ai agent will
+                    respond.
+                  </p>
+                </div>
               </div>
             </div>
-            <div className="flex gap-2 items-start">
-              <div className="p-2 rounded-full border-[#2d2d2d] border-[2px] bg-[#2b2b2b]">
-                <FileTextIcon color="#a22fe0" />
-              </div>
-              <div className="flex flex-col">
-                <h3 className="font-semibold">Summary</h3>
-                <p className="text-muted-foreground text-sm">
-                  Generate a description for your video using AI.
-                </p>
-              </div>
-            </div>
-            <div className="flex gap-2 items-start">
-              <div className="p-2 rounded-full border-[#2d2d2d] border-[2px] bg-[#2b2b2b]">
-                <Bot color="#a22fe0" />
-              </div>
-              <div className="flex flex-col">
-                <h3 className="font-semibold">AI Agent</h3>
-                <p className="text-muted-foreground text-sm">
-                  Viewers can ask questions on your video and ai agent will
-                  respond.
-                </p>
-              </div>
-            </div>
-          </div>
-            ) : ( plan === "FREE" ? (
-              <div>
-                Ai features are not enabled yet for this video.
-              </div>
-            ): (<AiChatbot />)
-          )
-          }
+          ) : plan === "FREE" ? (
+            <div>AI features are not enabled yet for this video.</div>
+          ) : (
+            <AiChatbot  videoId={videoId}/>
+          )}
         </div>
       </div>
     </TabsContent>
   );
 };
 
-const AiChatbot = (videoId: string) => {
-  const [messages , setMessages] = useState<Array<{role: 'user' | 'assistant', content: string}>>([]);
-  
-  const { register, onFormSubmit, isPending, reset } = useAIChat(videoId);
-  const handleQuestion = async (question: string) => {
-    try {
-     const response = await axios.post(`chat/${videoId}`, {
-      question: question
-    });
-
-    if(response.status !== 200) {
-      toast.error("Something went wrong. Please try again.");
-    }
-
-    if (response.status === 200) {
-      const answer = response.data.answer;
-      const question = response.data.question;
-
-      setMessages((messages) => [...messages, {role: 'user', content: question}, {role: 'assistant', content: answer}]);
-    }
-    } catch (error) {
-      toast.error("Something went wrong. Please try again.");
-    }
-   
-  }
-  return (
-    <div className="p-5 bg-[#1D1D1D] rounded-xl flex flex-col gap-y-10 h-[80vh]">
-      <div className="flex-1 overflow-auto space-y-3 p-2">
-       {
-        messages.map((message, index) => (
-          <div key = {index + 1} className={`flex ${message.role === 'user' ? "justify-end" : "justify-start"}`}>
-            <div className={`px-4 py-2 rounded-2xl max-w-[75%] text-sm ${
-              message.role === "user" ? "bg-green-500 text-white rounded-br-none"
-              : "bg-gray-200 dark:bg-gray-700 dark:text-white rounded-bl-none"
-            }`}>
-              {message.content}
-            </div>
-          </div>
-        ))
-       }
-      </div>
-    </div>
-  )
-}
-
 export default AiTools;
- 
